1696326f9d64d989c4835ee2fb9c31d806f1bda2
Milind <m.milind15@gmail.com>
2023-06-02 00:24:02 +0530

    chore: Stocks displayed

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,32 +1,51 @@
-import React from 'react';
-import Stock from './Stock';
+import React, { useEffect, useState } from 'react'
+import Stock from './Stock'
+import axios from 'axios'
 
 const MainSection = () => {
+  const [companies, setCompanies] = useState([])
+
+  useEffect(async () => {
+    await axios
+      .get(
+        `${import.meta.env.VITE_NEXT_PUBLIC_SERVER_URL}/stockastic/company`,
+        {
+          headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('jwt'),
+          },
+        }
+      )
+      .then((e) => {
+        const status = e.data.status
+        if (status === 'fail') {
+          alert(e.data.err)
+        } else {
+          setCompanies(e.data.companies)
+        }
+        return
+      })
+      .catch((e) => {})
+    return
+  }, [])
+
   return (
-    <div className="bg-[#1E1B1E] mx-10 rounded-3xl my-10 h-full">
-      <div className="flex flex-row items-center justify-around py-3 font-montaga text-white text-extrabold text-md px-[7%]">
-        <h1 className="flex-1">STOCK NAME</h1>
-        <h1 className="flex-1">VOLUME AVAILABLE</h1>
-        <h1 className="flex-1">PRICE</h1>
-        <h1 className="flex-1">Quantity</h1>
-        <h1 className="bg-white px-10 py-3 text-black rounded-2xl">Amount Left: <span>10000/- </span></h1>
+    <div className='bg-[#1E1B1E] mx-10 rounded-3xl my-10 h-full'>
+      <div className='flex flex-row items-center justify-around py-3 font-montaga text-white text-extrabold text-md px-[7%]'>
+        <h1 className='flex-1'>STOCK NAME</h1>
+        <h1 className='flex-1'>VOLUME AVAILABLE</h1>
+        <h1 className='flex-1'>PRICE</h1>
+        <h1 className='flex-1'>Quantity</h1>
+        <h1 className='bg-white px-10 py-3 text-black rounded-2xl'>
+          Amount Left: <span>10000/- </span>
+        </h1>
       </div>
-      <div className="bg-[#FE45RG] px-5 py-5">
-        <Stock />
-        <Stock />
-        <Stock />
-        <Stock />
-        <Stock />
-        <Stock />
-        <Stock />
-        <Stock />
-        <Stock />
-        <Stock />
-        <Stock />
-        <Stock />
+      <div className='bg-[#FE45RG] px-5 py-5'>
+        {companies.map((company) => (
+          <Stock company={company} />
+        ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default MainSection;
+export default MainSection
--- src/components/Stock.jsx
+++ src/components/Stock.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import CountTracker from './CountTracker';
 
-const Stock = () =>{
+const Stock = (props) =>{
   return(
     <div>
        <div className="flex flex-row items-center justify-around bg-[#303030] rounded-3xl text-white font-montaga py-3 my-3">
-         <div className="flex flex-row items-center justify-center">
+         <div className="flex flex-row items-center justify-center min-w-[10vw]">
            <img
              className="h-[40px] w-[50px]"
               src="./stockastic_logo.svg" />
-           <p>RELIANCE</p>
+           <p>{props.company.name}</p>
          </div>
          <div>
-           <p>70000</p>
+           <p>{props.company.volume}</p>
          </div>
-           <p>750/- </p>
+           <p>{props.company.price.toFixed(2)}</p>
            <CountTracker />
          <div className="">
            <button className="font-montaga py-2 px-5 rounded-xl bg-[#3DB042] w-[100px] mx-1">BUY</button>
