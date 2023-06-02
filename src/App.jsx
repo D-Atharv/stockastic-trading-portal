b0c74452581e146ed142f3949b131086a4471fda
Shantanu <shantanukhoslas@gmail.com>
2023-06-02 13:01:29 +0530

    added signin portal

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import MainSection from './components/MainSection';
 import MyPortfolio from './components/MyPortfolio';
 import PortfolioNavbar from './components/PortfolioNavbar';
+import SignIn from './components/Signin';
 
 
 const App = () => {
@@ -29,6 +30,7 @@ const App = () => {
   return (
     <div className="bg-[#1E1E1E] h-full">
       <Routes>
+        <Route path='/signin' element={<SignIn />} />
         <Route path="/" element={<TradingPortal />} />
         <Route path="/portfolio" element={<Portfolio />} />
       </Routes>
